7d4de6cd14872688cb3a34fb682bc7397c524c7b
Alive <aissatoubarry62355@gmail.com>
2025-03-23 22:39:35 +0100

    test8

--- src/main/java/fr/miage/toulouse/paoloamelaise/components/Competence.java
+++ src/main/java/fr/miage/toulouse/paoloamelaise/components/Competence.java
@@ -13,81 +13,29 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Mission {
-    private int id;
-    private String description;
-    private Date dateDebut;
-    private int duree;
-    private int nbParticipants;
-    private List<Competence> competencesRequises;
-    private List<Employe> participants;
-    private static List<Mission> listeMission = new ArrayList<>();
+import java.util.List;
+import java.util.ArrayList;
 
-    public Mission(int id, String description, Date dateDebut, int duree) {
+public class Competence {
+    private String id;
+    private String nom;
+    private static List<Competence> listeCompetence = new ArrayList<>();
+
+    public Competence(String id, String nom) {
         this.id = id;
-        this.description = description;
-        this.dateDebut = dateDebut;
-        this.duree = duree;
-        this.competencesRequises = new ArrayList<>();
-        this.participants = new ArrayList<>();
-    }
-
-    public void participerMission(Employe employe) {
-        participants.add(employe);
-        System.out.println(employe.getNom() + " participe maintenant à la mission " + description);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Date getDate() {
-        return dateDebut;
-    }
-
-    public int getDuree() {
-        return duree;
+        this.nom = nom;
     }
     
-    public List<Competence> getListeCompetences() {
-        return competencesRequises;
+    public String getId() {
+        return this.id;
     }
     
-    public static List<Mission> getListeMission() {
-        return listeMission;
-    }
-
-    public List<Employe> getListeEmploye() {
-        return participants;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setDate(Date date) {
-        this.dateDebut = date;
-    }
-
-    public void setDuree(int duree) {
-        this.duree = duree;
+    public static List<Competence> getListeCompetence() {
+        return listeCompetence;
     }
 
     @Override
     public String toString() {
-        return "ID: " + id + ", Description: " + description + ", Date Début: " + dateDebut + ", Durée: " + duree;
-    }
-
-    void setNbParticipants(int nbParticipants) {
-        this.nbParticipants = nbParticipants;
-    }
-
+        return "ID: " + this.id + ", Nom: " + this.nom;
+    }   
 }
